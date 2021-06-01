f4a8baa0ce652fdb3c050e8db90a4af2e56a3bc1
agnisnafaz <75403090+agnisnafaz@users.noreply.github.com>
2021-06-01 14:10:10 +0700

    migration table registers

--- database/migrations/2021_06_01_070747_create_registers_table.php
+++ database/migrations/2021_06_01_070747_create_registers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegisterTable extends Migration
+class CreateRegistersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -31,6 +31,6 @@ class CreateRegisterTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('register');
+        Schema::dropIfExists('registers');
     }
 }
